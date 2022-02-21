e4f8a6c292cd6eabb8e6422cde4b3bb5199ef16f
Sang Le <57263682+sqle157@users.noreply.github.com>
2022-02-20 22:02:27 -0700

    try to update the speed of fetching new advice

--- index.js
+++ index.js
@@ -4,22 +4,20 @@ let advice_id = document.querySelector("#advice--id");
 let advice_text = document.querySelector(".app__content--quote");
 
 // Get the advice from the API
-const getAdvice = () => {
-	fetch("https://api.adviceslip.com/advice")
+const getAdvice = async () => {
+	await fetch("https://api.adviceslip.com/advice", {
+		method: "GET",
+		cache: "no-cache",
+	})
 		.then((response) => response.json())
 		.then((data) => data.slip)
 		.then((quote) => {
-			// console.log(data);
+			// console.log(quote);
+
 			advice_id.textContent = quote.id;
-			advice_text.textContent = "“" + `${quote.advice}` + "”";
-		})
-		.catch((error) => {
-			console.log(error);
+			advice_text.textContent = `“${quote.advice}”`;
 		});
 };
-
-// Run getAdvice function on page load
-window.onload = getAdvice;
 
 // Add event listener to get new advice
 dice.addEventListener("click", getAdvice);
